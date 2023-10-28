a50465dbb8b1a4ff617efa5d3deb5566a0c4721f
emrankhnss <atdimla@gmail.com>
2023-10-28 18:16:38 +0600

    * Log added... so that you can see the time and priority difference

--- employee/src/main/java/com/employee/service/EmployeeService.java
+++ employee/src/main/java/com/employee/service/EmployeeService.java
@@ -2,6 +2,8 @@ package com.employee.service;
 
 import com.employee.entity.Employee;
 import com.employee.repo.EmployeeRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -21,38 +23,42 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepo repo;
 
-    Object target;
+    Logger logger = LoggerFactory.getLogger(EmployeeService.class);
 
     @Async
     public CompletableFuture<List<Employee>> saveEmployees(MultipartFile file) throws Exception {
         long start = System.currentTimeMillis();
-        List<Employee> Employees = parseCSVFile(file);
-        Employees = repo.saveAll(Employees);
+        List<Employee> employees = parseCSVFile(file);
+        logger.info("saving list of users of size {}", employees.size(), "" + Thread.currentThread().getName());
+        employees = repo.saveAll(employees);
         long end = System.currentTimeMillis();
-        return CompletableFuture.completedFuture(Employees);
+        logger.info("Total time {}", (end - start));
+        return CompletableFuture.completedFuture(employees);
     }
     @Async
     public CompletableFuture<List<Employee>> findAllEmployees(){
+        logger.info("get list of user by "+Thread.currentThread().getName());
         List<Employee> Employees = repo.findAll();
         return CompletableFuture.completedFuture(Employees);
     }
 
     private List<Employee> parseCSVFile(final MultipartFile file) throws Exception {
-        final List<Employee> Employees = new ArrayList<>();
+        final List<Employee> employees = new ArrayList<>();
         try {
             try (final BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
                 String line;
                 while ((line = br.readLine()) != null) {
                     final String[] data = line.split(",");
-                    final Employee Employee = new Employee();
-                    Employee.setName(data[0]);
-                    Employee.setEmail(data[1]);
-                    Employee.setGender(data[2]);
-                    Employees.add(Employee);
+                    final Employee employee = new Employee();
+                    employee.setName(data[0]);
+                    employee.setEmail(data[1]);
+                    employee.setGender(data[2]);
+                    employees.add(employee);
                 }
-                return Employees;
+                return employees;
             }
         } catch (final IOException e) {
+            logger.error("Failed to parse CSV file {}", e);
             throw new Exception("Failed to parse CSV file {}", e);
         }
     }
